ee6f5401ef1c2fac264dc73dde0d9956340bad05
BeatrizSuyo <beatrizsuyo@gmail.com>
2022-05-23 18:57:04 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import validator from './validator.js';
 
-console.log(validator);
+//llamar al boton//
+let buton = document.getElementById("validar");//llamar la buton
+buton.addEventListener('click',()=>{
+  creditCardNumber();
+});
+//empiezo con variable credicard//
+function creditCardNumber() {
+  let creditcard =document.getElementById("Creditcard").value;
+
+  let arrCreditcard = Array.from(creditcard);
+  let totalPares = 0;
+  let totalimpares = 0;
+
+  for(let i=0; i < arrCreditcard.length; i++){
+    if(i%2 ===0 ){
+        console.log(typeof(arrCreditcard[i]))
+      if(arrCreditcard[i]*2 >9){
+        totalPares += arrCreditcard[i]*2-9;
+        }
+      else{
+        totalPares += arrCreditcard[i]*2;
+        }
+     }
+      else{
+        totalimpares += arrCreditcard[i]*1;
+        }
+    
+    }
+   console.log(totalPares,totalimpares)
+  if((totalPares + totalimpares)%10 ===0 ){
+    
+    alert(" TARJETA  VALIDA")
+    
+  }
+  else{
+    alert(" TARJETA NO VALIDA")
+  }
+  
+}
+
+//=== son iguales
+
+ 
+
+ console.log(validator);
